6491520fdea56d75301d92e6c971ba945dad7b8e
aleksandr zanko <aleksandr.zanko@enway.com>
2021-10-30 20:24:03 +0300

    Added checkbox component

--- frontend/components/ace/checkbox/checkbox.js
+++ frontend/components/ace/checkbox/checkbox.js
@@ -5,4 +5,80 @@ import './checkbox.scss';
 export default class Checkbox extends LightningElement {
 
 
+    @api checked = "off";
+    @api required;
+    @api name;
+    @api label;
+    @api requiredMessage;
+    @api disabled;
+
+    get cssComponent() {
+        let result = "ace-checkbox";
+        if(this.disabled){
+            result += " ace-checkbox_disabled";
+        }else{
+            if(this.required && this.usedByUser && !this.isValid()){
+                result += " ace-checkbox_error";
+            }
+        }
+        return result;
+    }
+
+    get iconType(){
+        let type = 'checkbox-off';
+        if(this.usedByUser){
+            if(this.isValid()){
+                if(this.checked === "on"){
+                    type = 'checkbox-on';
+                }
+            }else{
+                type = 'checkbox-error';
+            }
+        }else if(this.disabled){
+            type = this.checked === 'on' ? 'checkbox-disabled-on' : 'checkbox-disabled';
+        }
+        return type; 
+    }
+
+    usedByUser = false;
+    
+    clickHandler(){
+        if(!this.disabled){
+            this.checked = this.checked === "off" ? "on" : "off";
+            this.validate();
+
+            this.dispatchEvent(new CustomEvent('acecheckbox',{
+                detail:{
+                    name: this.name,
+                    checked: this.checked === 'on',
+                    isValid: this.isValid()
+                }
+            }));
+        }
+    }
+
+    @api validate(){
+        if(!this.disabled){
+            this.usedByUser = true;
+        }
+        return this.isValid();
+    }
+
+    @api isValid(){
+        if(this.required){
+            return this.checked === "on";
+        }else{
+            return true;
+        }
+    }
+
+    connectedCallback(){
+        if(this.checked === "on"){
+            this.validate();
+        }
+    }
+
+    renderedCallback(){
+        this.template.querySelector('.ace-checkbox__label').innerHTML = this.label;
+    }
 }
--- frontend/components/ace/icon/icon.js
+++ frontend/components/ace/icon/icon.js
@@ -3,6 +3,12 @@ import TMPL_CROSS from './tmpl_cross.html';
 import TMPL_SUCCESS from './tmpl_success.html';
 import TMPL_WARNING from './tmpl_warning.html';
 import TMPL_ARROW_DOWN from './tmpl_arrow_down.html';
+import TMPL_CHECKBOX_OFF from './tmpl_checkbox_off.html';
+import TMPL_CHECKBOX_ON from './tmpl_checkbox_on.html';
+import TMPL_CHECKBOX_ERROR from './tmpl_checkbox_error.html';
+import TMPL_CHECKBOX_DISABLED from './tmpl_checkbox_disabled.html';
+import TMPL_CHECKBOX_DISABLED_ON from './tmpl_checkbox_disabled_on.html';
+
 import './icon.scss';
 import { LightningElement, api} from 'lwc';
 
@@ -10,7 +16,12 @@ const ICONS = {
     'cross': TMPL_CROSS,
     'success': TMPL_SUCCESS,
     'warning': TMPL_WARNING,
-    'arrow-down': TMPL_ARROW_DOWN
+    'arrow-down': TMPL_ARROW_DOWN,
+    'checkbox-off': TMPL_CHECKBOX_OFF,
+    'checkbox-on': TMPL_CHECKBOX_ON,
+    'checkbox-error': TMPL_CHECKBOX_ERROR,
+    'checkbox-disabled': TMPL_CHECKBOX_DISABLED,
+    'checkbox-disabled-on': TMPL_CHECKBOX_DISABLED_ON
 }
 
 export default class Icon extends LightningElement {
