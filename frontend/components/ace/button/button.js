40bfd299ecd7304b8fc38da7b79f170a20197177
aleksandr zanko <aleksandr.zanko@enway.com>
2021-11-04 15:59:48 +0300

    Add radio buttons and button

--- frontend/components/ace/button/button.js
+++ frontend/components/ace/button/button.js
@@ -1,9 +1,26 @@
 import './button.scss';
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
+
+const VARIANTS = {
+    PRIMARY: 'primary'
+}
 export default class Button extends LightningElement {
+    @api label;
+    @api disabled;
+    @api variant;
 
-    pressed(){
-        console.log('Please stop click me!');
+    get cssClass(){
+        let result = "ace-button";
+
+        if(this.disabled){
+            result += " ace-button_disabled";
+        }
+
+        if(this.variant === VARIANTS.PRIMARY){
+            result += " ace-button_primary";
+        }
+
+        return result;
     }
- }
+}
--- frontend/components/ace/radio/radio.js
+++ frontend/components/ace/radio/radio.js
@@ -4,5 +4,63 @@ import './radio.scss';
 
 export default class Radio extends LightningElement {
 
+    @api options = []
+    @api required
+    @api requiredMessage
+    @api value
+    @api label
+    @api name
+    @api disabled
+    @api vertical
+
+    usedByUser
+
+    get opts(){
+        return this.options.map(item => {
+            return {...item, selected: item.value === this.value}
+        })
+    }
+
+    get cssContainer(){
+        let result = "ace-radio";
+
+        if(this.vertical){
+            result += " ace-radio_vertical";
+        }
+
+        if(this.disabled){
+            result += " ace-radio_disabled";
+        }
+        
+        if(this.usedByUser && !this.isValid()){
+            result += " ace-radio_error";
+        }
+
+        return result;
+    }
+
+    @api validate(){
+        this.usedByUser = true
+        return this.isValid()
+    }
+
+    @api isValid(){
+        return this.required ? !!this.value : true
+    }
+
+    changeHandler(event){
+        if(!this.disabled){
+            this.value = event.detail
+            this.usedByUser = true
+            this.dispatchEvent(new CustomEvent('aceradio',{
+                detail:{
+                    name: this.name,
+                    label: this.label,
+                    value: this.value,
+                    isValid: this.isValid()
+                }
+            }))
+        }
+    }
 
 }
